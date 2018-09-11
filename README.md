d7ea7c12ef7ded92749dda66268c936f4dee9e92
mide42 <demi_05@arcor.de>
2018-09-11 20:37:48 +0200

    README-Datei samt Screenshot dazu

---
+++ README.md
@@ -0,0 +1,19 @@
+# Android-App "Hallo DHBW"
+
+Source code for simple "Hello World!"-App (just shows static text) as project for "Android Studio".
+
+<br>
+
+----
+## Screenshots
+
+![Screenshot 1](screenshot_1.png)
+
+Screenshot was taken from emulator.
+
+<br>
+
+----
+# License
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License).
---
+++ app/src/main/java/de/mide/hallodhbw/MainActivity.java
@@ -0,0 +1,16 @@
+package de.mide.hallodhbw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+ /**
+  * This project is licensed under the terms of the BSD 3-Clause License.
+  */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
