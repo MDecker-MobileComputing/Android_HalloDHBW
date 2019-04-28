55be1879b5fa07193c9aa715c4d50e88eff2bb4d
mide42 <demi_05@arcor.de>
2019-04-28 22:06:17 +0200

    Upgrade auf Android Studio 3.4.

--- app/src/main/java/de/mide/hallodhbw/MainActivity.java
+++ app/src/main/java/de/mide/hallodhbw/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 
  /**
-  * Einfache "Hello World"-App, zeigt nur einen statischen String an.
+  * Einfache "Hello World"-App, zeigt nur einen statischen Text an.
   * <br><br>
   *
   * This project is licensed under the terms of the BSD 3-Clause License.
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
