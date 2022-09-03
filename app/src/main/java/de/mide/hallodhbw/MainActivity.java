9718da500026e1c6d31a1c59d7cd062f400b9f64
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-09-03 20:43:54 +0200

    Update auf Android Studio 2021.2.1 Patch 2

--- app/src/main/java/de/mide/hallodhbw/MainActivity.java
+++ app/src/main/java/de/mide/hallodhbw/MainActivity.java
@@ -3,20 +3,22 @@ package de.mide.hallodhbw;
 import android.app.Activity;
 import android.os.Bundle;
 
-
- /**
-  * Einfache "Hello World"-App, zeigt nur einen statischen Text an.
-  * <br><br>
-  *
-  * This project is licensed under the terms of the BSD 3-Clause License.
-  */
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
